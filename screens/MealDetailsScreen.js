919d1166efd5891569560d32e29844ac1a74dad5
Carlos Craig <carlos.craig09@gmail.com>
2023-06-10 16:02:14 -0700

    added tab views to details screen

--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -1,5 +1,12 @@
-import { Image, StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import {
+  Image,
+  StyleSheet,
+  Text,
+  View,
+  useWindowDimensions,
+} from "react-native";
+import { TabView, SceneMap, TabBar } from "react-native-tab-view";
 import { ScrollView } from "react-native-gesture-handler";
 import { useSelector } from "react-redux";
 import { WebView } from "react-native-webview";
@@ -8,12 +15,51 @@ import { InfoTag } from "../components/InfoTag";
 
 const MealDetailsScreen = (props) => {
   const { mealId } = props.route.params;
+  const layout = useWindowDimensions();
 
-  const meal = useSelector((state) =>
-    state.meals.meals.find((meal) => meal.id == mealId)
+  const [index, setIndex] = useState(0);
+  const [routes] = useState([
+    { key: "first", title: "Instructions" },
+    { key: "second", title: "Ingredients" },
+  ]);
+
+  const renderTabBar = (props) => (
+    <TabBar
+      {...props}
+      indicatorStyle={{ backgroundColor: "white" }}
+      style={{ backgroundColor: "green" }}
+    />
   );
 
-  console.log(">>>>Meal Detail", meal.analyzedInstructions[0].steps);
+  const meal = useSelector((state) =>
+    state.meals.searchResults.find((meal) => meal.id == mealId)
+  );
+  const FirstRoute = () => (
+    <WebView
+      textZoom={30}
+      style={{ backgroundColor: "#dcdcde" }}
+      containerStyle={{
+        marginTop: 20,
+        flex: 0,
+        height: 200,
+      }}
+      originWhitelist={["*"]}
+      source={{
+        html: `<p style="font-size: 50; line-height: 1.5; "> ${meal.analyzedInstructions[0].steps} </p`,
+      }}
+    />
+  );
+
+  const SecondRoute = () => (
+    <View style={{ flex: 1, backgroundColor: "#673ab7" }} />
+  );
+
+  const renderScene = SceneMap({
+    first: FirstRoute,
+    second: SecondRoute,
+  });
+
+  // console.log(">>>>Meal Detail", meal.analyzedInstructions[0].steps);
   return (
     <ScrollView style={styles.container}>
       <View style={styles.heading}>
@@ -51,25 +97,13 @@ const MealDetailsScreen = (props) => {
 				<Text style={styles.label}>Address: </Text>
 				<Text style={styles.value}>{meal.address}</Text>
 			</View>*/}
-      <View style={styles.group}>
-        <Text style={styles.label}>Instructions: </Text>
-        <WebView
-          textZoom={30}
-          style={{ backgroundColor: "#dcdcde" }}
-          containerStyle={{
-            marginTop: 20,
-            flex: 0,
-            height: 200,
-          }}
-          originWhitelist={["*"]}
-          source={{
-            html: `<p style="font-size: 50; line-height: 1.5; "> ${meal.instructions} </p`,
-          }}
-        />
-      </View>
-      <View style={styles.group}>
-        <Text style={styles.label}>Ingredients: </Text>
-      </View>
+      <TabView
+        renderTabBar={renderTabBar}
+        navigationState={{ index, routes }}
+        renderScene={renderScene}
+        onIndexChange={setIndex}
+        initialLayout={{ width: layout.width }}
+      />
     </ScrollView>
   );
 };
